4ba26779cc037abfe17a9b683d0e499550db6fb3
prankroker <prankroker2@gmail.com>
2026-03-24 18:30:57 +0200

    Styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {Music} from "lucide-react";
 
 function App() {
   const url = "https://api.lyrics.ovh/v1/";
@@ -27,58 +28,62 @@ function App() {
         throw new Error(`Response status: ${response.status}`);
       }
 
-      let x = await response.json();
+      const x = await response.json();
       setText(x.lyrics);
     } catch (error) {
+      console.error("Error fetching lyrics:", error);
       setText("Not Found!");
     }
   };
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center rounded-4xl bg-indigo-900 text-white max-w-xl max-h-5/12">
-        <h1 className="m-4 font-bold text-2xl">Lyrics Finder</h1>
-        <p className="mb-4">
-          Find lyrics by entering artist and title of the song
+      <div className="flex flex-col justify-center items-center rounded-4xl bg-[#1a1a1a] text-white max-w-2xl max-h-6/12 p-6 w-1/3">
+        <div className="bg-[#1DB954] rounded-full mb-2 flex items-center justify-center w-14 h-14 min-w-14 min-h-14">
+          <Music color="#000000" size={32}/>
+        </div>
+        <h1 className="mt-4 mr-4 ml-4 font-bold text-2xl text-white">Lyrics Finder</h1>
+        <p className="mb-6 text-[#a0a0a0]">
+          Search by artist and song title
         </p>
-        <form action="" method="get" className="table" onSubmit={handleSubmit}>
-          <div className="flex flex-col">
-            <div className="m-6 flex flex-col">
-              <label htmlFor="artist">Enter artist:</label>
+        <form action="" method="get" className="table w-full min-w-80 px-6" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="artist" className="text-[#a0a0a0] text-sm">Artist:</label>
               <input
                 id="artist"
                 name="artist"
                 value={artist}
                 onChange={handleArtist}
                 type="text"
-                placeholder="Enter artist"
-                className="border border-black rounded text-gray-400 focus:border-emerald-500 focus:outline-emerald-500"
+                placeholder="Eg. Beatles"
+                className="w-full p-3 rounded-lg bg-[#2a2a2a] text-[#a0a0a0] focus:border-[#1DB954] focus:outline-[#1DB954]"
                 required
               ></input>
             </div>
-            <div className="mb-6 ml-6 mr-6 flex flex-col">
-              <label htmlFor="title">Enter title:</label>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="title" className="text-[#a0a0a0] text-sm">Song title:</label>
               <input
                 id="title"
                 name="title"
                 value={title}
                 onChange={handleTitle}
                 type="text"
-                className="border border-black rounded text-gray-400 focus:border-emerald-500 focus:outline-emerald-500"
-                placeholder="Enter title"
+                className="w-full p-3 rounded-lg bg-[#2a2a2a] text-[#a0a0a0] focus:border-[#1DB954] focus:outline-[#1DB954]"
+                placeholder="Eg. Let It Be"
                 required
               ></input>
             </div>
             <button
               type="submit"
-              className="border border-black rounded bg-emerald-700 hover:bg-emerald-500 active:bg-emerald-600"
+              className="w-full mt-2 p-3 border border-[#a0a0a0] rounded bg-[#1a1a1a] hover:bg-[#2a2a2a] active:bg-[#2a2a2a]"
               onClick={handleClick}
             >
               Search
             </button>
           </div>
         </form>
-        <div id="output" className="m-6 overflow-auto scrollbar">
+        <div id="output" className="m-4 w-full h-1/2 text-[#a0a0a0] overflow-auto scrollbar">
           {text}
         </div>
       </div>
